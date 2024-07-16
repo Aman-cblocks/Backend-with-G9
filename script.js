4b2863f9916c5e9089ba1cd6dcac9a5b52ae720b
Aman <amancodingblocks@outlook.com>
2024-07-16 11:40:12 -0700

    lec_2

---
+++ script.js
@@ -0,0 +1,55 @@
+// // //Scopes
+// // // var a=100;
+// // {
+// // var a =200;
+// // }
+// // console.log(a)
+
+
+// // // let x=100;
+// // {
+// // let x =200;
+// // // console.log(x)
+// // }
+// // console.log(x)
+
+
+// debugger
+// var a="code";
+
+// {
+//     let b="2000"
+//     console.log(a)
+// }
+// console.log(a)
+
+
+// Datatypes
+
+//Numbers
+
+// var a=10000;
+// console.log(a,typeof a)
+
+// Methods in Numbers;
+
+
+// a=10.34555
+// console.log(a,typeof a)
+
+//Strings
+
+let a = "code"
+console.log(a.length)
+
+//Booleans
+//True/False
+//false
+//""
+//Null
+//undefined
+//0
+//Nan
+
+var bool = false
+console.log(Boolean(""))
